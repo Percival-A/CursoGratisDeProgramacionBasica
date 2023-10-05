74aabcee68dff28cf37814c99e665cd783d60171
Percival-A <98198294+Percival-A@users.noreply.github.com>
2023-10-05 14:29:28 +0000

    evento para escuchar carga de pagina

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,14 @@
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+
 function seleccionarMascotaJugador() {
     alert('Seleccionaste tu mascota')
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+//evento para escuchar cuando se carge la pagina, desplegar un mnsaje en consola y llamar a la funcion iniciarJuego
+window.addEventListener('load', () => {
+    console.log('La pagina se cargo correctamente')
+    iniciarJuego()
+})
