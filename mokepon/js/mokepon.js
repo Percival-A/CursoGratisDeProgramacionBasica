40c56e4d27f607266fd55ce16dbc406727c87dbf
Percival-A <98198294+Percival-A@users.noreply.github.com>
2023-10-05 15:04:50 +0000

    Escuchando eventos con JavaScript

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,10 +1,21 @@
+// varaibles
+let mascotaSeleccionada = ``
+
+//funciones
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
 function seleccionarMascotaJugador() {
-    alert('Seleccionaste tu mascota')
+    if (document.getElementById('Hipodoge').checked) {
+        mascotaSeleccionada = `Hipodoge`
+    } else if (document.getElementById('Capipepo').checked) {
+        mascotaSeleccionada = `Capipepo`
+    } else if (document.getElementById('Ratigueya').checked) {
+        mascotaSeleccionada = `Ratigueya`
+    }
+    alert(`seleccionaste a ${mascotaSeleccionada}`)
 }
 
 //evento para escuchar cuando se carge la pagina, desplegar un mnsaje en consola y llamar a la funcion iniciarJuego
