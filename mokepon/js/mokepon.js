bdbf40a9a5fdf62a19c3a60a171a923fb1a576fb
Percival-A <98198294+Percival-A@users.noreply.github.com>
2023-10-06 14:33:01 +0000

    addEventListener

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,3 @@
-// varaibles
-let mascotaSeleccionada = ``
-
 //funciones
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById('boton-mascota')
@@ -8,14 +5,18 @@ function iniciarJuego() {
 }
 
 function seleccionarMascotaJugador() {
-    if (document.getElementById('Hipodoge').checked) {
-        mascotaSeleccionada = `Hipodoge`
-    } else if (document.getElementById('Capipepo').checked) {
-        mascotaSeleccionada = `Capipepo`
-    } else if (document.getElementById('Ratigueya').checked) {
-        mascotaSeleccionada = `Ratigueya`
+    let inputHipodoge = document.getElementById('Hipodoge')
+    let inputCapipepo = document.getElementById('Capipepo')
+    let inputRatigueya = document.getElementById('Ratigueya')
+    if (inputHipodoge.checked) {
+        alert(`Seleccionaste a Hipodoge`)
+    } else if (inputCapipepo.checked) {
+        alert(`Seleccionaste a Capipepo`)    
+    } else if (inputRatigueya.checked) {
+        alert(`Seleccionaste a Ratigueya`)
+    } else {
+        alert(`No seleccioaste una mascota`)
     }
-    alert(`seleccionaste a ${mascotaSeleccionada}`)
 }
 
 //evento para escuchar cuando se carge la pagina, desplegar un mnsaje en consola y llamar a la funcion iniciarJuego
